7934b920077b8a432df1918d3adfe2688a2c5a22
Onimae <manufelipearias@gmail.com>
2023-05-21 23:46:38 -0500

    adding to do item component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { ToDoCounter } from "./ToDoCounter";
 import { ToDoSeach } from "./ToDoSearch";
 import { ToDoList } from "./ToDoList";
 import { CreateToDoButton } from "./CreateToDoButton";
+import { ToDoItem } from "./ToDoItem";
+
 import "./App.css";
 
 function App() {
@@ -19,16 +21,6 @@ function App() {
 
       <CreateToDoButton />
     </div>
-  );
-}
-
-function ToDoItem() {
-  return (
-    <li>
-      <span>✓</span>
-      <p>first task</p>
-      <span>×</span>
-    </li>
   );
 }
 
