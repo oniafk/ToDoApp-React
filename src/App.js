21178bb88c76161a6d797df5ec6bb5a9326fbb5e
Onimae <manufelipearias@gmail.com>
2023-05-25 02:57:03 -0500

    adding styles todo app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import { ToDoCounter } from "./ToDoCounter";
-import { ToDoSeach } from "./ToDoSearch";
-import { ToDoList } from "./ToDoList";
-import { CreateToDoButton } from "./CreateToDoButton";
-import { ToDoItem } from "./ToDoItem";
+import { ToDoCounter } from "./components/ToDoCounter";
+import { ToDoSeach } from "./components/ToDoSearch";
+import { ToDoList } from "./components/ToDoList";
+import { CreateToDoButton } from "./components/CreateToDoButton";
+import { ToDoItem } from "./components/ToDoItem";
+import React from "react";
 
-import "./App.css";
+const defaultToDos = [
+  { text: "Finish ToDo App", completed: true },
+  { text: "keep learning react", completed: false },
+  { text: "play apex", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <ToDoCounter />
+    <React.Fragment>
+      <ToDoCounter completed={3} total={5} />
 
       <ToDoSeach />
 
       <ToDoList>
-        <ToDoItem />
-        <ToDoItem />
-        <ToDoItem />
+        {defaultToDos.map((toDo) => (
+          <ToDoItem
+            key={toDo.text}
+            text={toDo.text}
+            completed={toDo.completed}
+          />
+        ))}
       </ToDoList>
 
       <CreateToDoButton />
-    </div>
+    </React.Fragment>
   );
 }
 
--- src/ToDoSearch.js
+++
@@ -1,5 +0,0 @@
-function ToDoSeach() {
-  return <input placeholder="Learn React" />;
-}
-
-export { ToDoSeach };
